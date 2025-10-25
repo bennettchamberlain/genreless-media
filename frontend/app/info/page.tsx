408f6707fe8e41bb3dbe1e4c95187253cfff7efc
bennettchamberlain <chamberlain.bennett@gmail.com>
2025-10-25 08:05:38 +0200

    refactor: update layout styles in InfoPage for footer to hug bottom of the screen

--- frontend/app/info/page.tsx
+++ frontend/app/info/page.tsx
@@ -5,8 +5,8 @@ export default function InfoPage() {
   return (
     <div className="flex flex-col min-h-screen bg-[#dadad6]">
       <InfoHeader />
-      <div style={{ position: "relative", paddingTop: "50px" }}>
-        <main className="max-w-6xl mx-auto px-4 pt-8 pb-32">
+      <div className="flex-1 flex flex-col" style={{ position: "relative", paddingTop: "50px" }}>
+        <main className="max-w-6xl mx-auto px-4 pt-8 pb-48 flex-1">
           <div className="text-[1.6rem] md:text-[2.5rem] leading-snug font-[Helvetica] font-normal text-[#1a21a5]" style={{ wordBreak: 'break-word', transform: 'scaleY(1.14)', transformOrigin: 'left top' }}>
             genreless media is a production company founded by <a href="https://www.instagram.com/jackierad/" target="_blank" rel="noopener noreferrer" className="underline hover:text-[#fe2e2e] transition-colors duration-200">Jackie Radinsky</a> in Los Angeles, with roots worldwide. It was conceived out of a reverence for humanity, ephemera, & nature.<br /><br />
             We create commercials, music videos, docs, physical products, + everything in between. We&apos;re firm believers in the power of <span className="italic">sincere</span> art to illuminate a brighter future and to tether community.<br /><br />
